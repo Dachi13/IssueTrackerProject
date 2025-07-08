9db0b5b02d605b01e0c1e043e375b2aabcc1feb4
Dachi <Lekborashvili.dachi@gmail.com>
2025-07-08 17:42:04 +0400

    moved usings into global usings file

--- IssueTracker/Controllers/HomeController.cs
+++ IssueTracker/Controllers/HomeController.cs
@@ -1,7 +1,3 @@
-using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using IssueTracker.Models;
-
 namespace IssueTracker.Controllers;
 
 public class HomeController(ILogger<HomeController> logger) : Controller
